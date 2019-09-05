4612ac71d3fcaefea16963b5d90cd29ef14c8352
martinpluralsight <martinpluralsight@gmail.com>
2019-09-05 11:15:59 +0200

    nytt typ. Movie model

--- FilmProj/FilmProj/Controllers/HomeController.cs
+++ FilmProj/FilmProj/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace FilmProj.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Movies()
+        {
+            return View();
+        }
     }
 }
--- FilmProj/FilmProj/Data/Migrations/20190905091402_firstMovie.Designer.cs
+++ FilmProj/FilmProj/Data/Migrations/20190905091402_firstMovie.Designer.cs
@@ -4,14 +4,16 @@ using FilmProj.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FilmProj.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190905091402_firstMovie")]
+    partial class firstMovie
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
