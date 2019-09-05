d0292379686ded269fae041b882da7233081930b
martinpluralsight <martinpluralsight@gmail.com>
2019-09-05 13:19:07 +0200

    random stage minns ej innan mergetest

--- FilmProj/FilmProj/Data/Migrations/20190905111103_jadå.Designer.cs
+++ FilmProj/FilmProj/Data/Migrations/20190905111103_jadå.Designer.cs
@@ -4,14 +4,16 @@ using FilmProj.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FilmProj.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190905111103_jadå")]
+    partial class jadå
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
